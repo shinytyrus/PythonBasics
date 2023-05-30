d68650e077062055fe18565f932e736ee728387f
tyruscaldeira <tyrus.caldeira@shinesolutions.com>
2023-05-30 14:53:43 +1000

    Updated the lists and dictionaries basics topics.

--- basics/basics_dictionaries.py
+++ basics/basics_dictionaries.py
@@ -1,3 +1,37 @@
 """
     This file covers a basic overview of Dictionaries in Python
 """
+
+# Creating a Dictionary
+dict_a = {"test": 1}
+dict_b = dict(test=1, test_2=2)
+dict_c = dict([("test", 1), ("test2", 2)])
+print(type(dict_a))
+print(type(dict_b))
+print(type(dict_c))
+
+# common list functionalities
+length_dict = len(dict_b)
+get_key_dict = dict_b.get("test")
+keys_dict = dict_b.keys()
+view_dict = dict_b.items()
+values_dict = dict_b.values()
+list_dict = list(dict_b)
+key_value_1_dict = dict_b["test"]
+print(length_dict)
+print(get_key_dict)
+print(keys_dict)
+print(view_dict)
+print(values_dict)
+print(list_dict)
+print(key_value_1_dict)
+
+# List Manipulation - List objects are mutable
+dict_b["test"] = "helloWorld!"
+print(dict_b)
+del dict_b["test"]
+print(dict_b)
+dict_b.update({"abc": 2})
+print(dict_b)
+dict_b.pop("abc")
+print(dict_b)
--- basics/basics_lists.py
+++ basics/basics_lists.py
@@ -1,3 +1,37 @@
 """
     This file covers a basic overview of Lists in Python
 """
+
+# Creating a List
+list_a = [1, 2, 3]
+list_b = list((1, 2, 3))
+list_c = list("testing")
+print(type(list_a))
+print(type(list_b))
+print(type(list_c))
+
+# common list functionalities
+max_list = max(list_b)
+min_list = min(list_b)
+length_list = len(list_b)
+concatenate_lists = list_a + list_b
+occurrence_count_list = list_a.count(2)
+print(max_list)
+print(min_list)
+print(length_list)
+print(concatenate_lists)
+print(occurrence_count_list)
+
+# List Manipulation - List objects are mutable
+list_a[0] = 2
+print(list_a)
+list_a.append(4)
+print(list_a)
+list_a.insert(1, 10)
+print(list_a)
+list_a.reverse()
+print(list_a)
+list_a.pop()
+print(list_a)
+list_a.remove(2)
+print(list_a)
